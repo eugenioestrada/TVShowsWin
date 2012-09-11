cefbb48b0c74dc201996e1879e65738acc701710
Eugenio Estrada <mail@eugenioestrada.es>
2012-09-11 22:07:49 +0200

    MessengerService class and interface created

---
+++ sources/TVShowsWin/Services/Messenger/IMessengerService.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IMessengerService.cs" company="TVShowsWin">
+// GNU General Public License see http://www.gnu.org.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TVShowsWin.Services.Messenger
+{
+    /// <summary>
+    /// Interface of the Messenger Service
+    /// </summary>
+    public interface IMessengerService
+    {
+    }
+}
---
+++ sources/TVShowsWin/Services/Messenger/MessengerService.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MessengerService.cs" company="TVShowsWin">
+// GNU General Public License see http://www.gnu.org.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TVShowsWin.Services.Messenger
+{
+    /// <summary>
+    /// Implementation of the Messenger Service
+    /// </summary>
+    public class MessengerService : IMessengerService
+    {
+    }
+}
