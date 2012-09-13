ee43e459151ea52084fdf8ea3b56d37ab633c8b8
Eugenio Estrada <mail@eugenioestrada.es>
2012-09-13 22:27:27 +0200

    The TVDB Provider added uncompletely

---
+++ sources/TVShowsWin/Providers/TheTVDBProvider.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TheTVDBProvider.cs" company="TVShowsWin">
+// GNU General Public License see http://www.gnu.org.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TVShowsWin.Providers
+{
+    /// <summary>
+    /// The TVDB Provider
+    /// </summary>
+    public class TheTVDBProvider
+    {
+        /// <summary>
+        /// The TVDB Api Key
+        /// </summary>
+        public static readonly string ApiKey = "2AEC8D238223C926";
+    }
+}
